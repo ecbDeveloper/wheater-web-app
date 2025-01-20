53ca36dcd1fc3f373b57577bb4bed5d28df62a2b
Eduardo Castilho <developer.ecb@gmail.com>
2025-01-20 03:39:12 -0300

    removing fetch settings

--- web/static/scripts.js
+++ web/static/scripts.js
@@ -21,15 +21,9 @@ function updateButton() {
 async function fetchWeather(event) {
     event.preventDefault();
     const location = input.value
-    const fetchSettings = {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    }
+
     try {
-        const resp = await fetch(`http://localhost:8002/getweather/${location}`, fetchSettings);
+        const resp = await fetch(`http://localhost:8002/getweather/${location}`);
         if (!resp.ok) {
             throw new Error(`Status: ${resp.status}`);
         }
@@ -45,12 +39,12 @@ async function fetchWeather(event) {
 
 
 function template(data) {
-    const weather = data['weather'][0]
-    const cityName = data['name']
-    const temperature = data['main']['temp']
-    const humidity = data['main']['humidity']
-    const description = weather['description']
-    const icon = weather['icon']
+    const weather = data.weather[0]
+    const cityName = data.name
+    const temperature = data.main.temp
+    const humidity = data.main.humidity
+    const description = weather.description
+    const icon = weather.icon
 
     const cityHTML = document.querySelector('.city')
     const tempHTML = document.querySelector('.temperature')
